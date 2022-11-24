8dbaf6d5c366802550533d278fc014b09acded89
Silas inegbe <kingssilas18@gmail.com>
2022-11-24 03:42:20 +0100

    Create others.c

---
+++ main/others.c
@@ -0,0 +1,110 @@
+#include "main.h"
+
+/**
+ * print_HEX_aux - prints an hexgecimal number
+ * @num: number to print
+ * Return: counter
+ */
+int print_HEX_aux(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * print_hex_aux - prints an hexgecimal number
+ * @num: arguments
+ * Return: counter
+ */
+int print_hex_aux(unsigned long int num)
+{
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * print_exclusive_string - print exclusuives string
+ * @val: argumen t
+ * Return: the length of the string
+ */
+
+int print_exclusive_string(va_list val)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + print_HEX_aux(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
